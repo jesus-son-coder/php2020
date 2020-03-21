c886303e578e85db69e17c433fb8ff67eba7d787
hseka <sekaherve@gmail.com>
2020-03-21 22:53:55 +0100

    Fonctionnalité de recherche et d'affichage des résultats avec exploitation en JavaScript des données reçues de l'API Youtube v3

--- 06_JavaScript/20200320_01_Youtube_Api_Get_Request/js/script.js
+++ 06_JavaScript/20200320_01_Youtube_Api_Get_Request/js/script.js
@@ -28,4 +28,94 @@ $(function() {
             },400, function(){});
         }
     });
-})
+
+    $('#search-form').submit(function(e) {
+        e.preventDefault();
+    })
+});
+
+function search() {
+    // Clear results :
+    $('#results').html('');
+    $('#buttons').html('');
+
+    // Get Form input :
+    q = $('#query').val();
+
+    // Run GET Request on API :
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            type: 'video',
+            key: 'AIzaSyAuwd5unHZnInjGb5LJzajnxTcWCLsJd6I'
+        },
+        function(data) {
+            var nextPageToken = data.nextPageToken;
+            var previousPageToken = data.previousPageToken;
+
+            // Log Data :
+            console.log(data);
+
+            $.each(data.items, function(i, item) {
+                // Get Output :
+                var output = getOutput(item);
+
+                // Display Results :
+                $('#results').append(output);
+            });
+
+            var buttons = getButtons(nextPageToken, previousPageToken);
+
+            // Display Buttons :
+            $('#buttons').append(buttons);
+
+        }
+    );
+}
+
+// Build Output :
+function getOutput(item) {
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.publishedAt;
+
+    // Build Output String :
+    var output = '<li>' + 
+    '<div class="list-left">' + 
+    '<img src="' + thumb + '" />' +
+    '</div>' +
+    '<div class="list-right">' +
+    '<h3>' + title + '</h3>' +
+    '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + ' </small>' +
+    '<p>' + description + '</p>' +
+    '</div>' +
+    '</li>' +
+    '<div class="clearfix"></div>' +
+    '';
+
+    return output;
+}
+
+
+// Build the buttons :
+function getButtons(nextPageToken, previousPageToken) {
+    if(!previousPageToken) {
+        var btnoutput = '<div class="button-container">' +
+            '<button id="next-button" class="btn btn-info paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            ' onclick="nextPage();">Next Page</button></div>';
+        ;
+    } else {
+        var btnoutput = '<div class="button-container">' +
+            '<button id="previous-button" class="btn btn-info paging-button" data-token="' + previousPageToken + '" data-query="' + q + '"' +
+            ' onclick="previousPage();">previous Page</button>' +
+
+            '<button id="next-button" class="btn btn-info paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            ' onclick="nextPage();">Next Page</button></div>';
+    }
+
+    return btnoutput;
+}
