fd6c91b0fbf3bec234f91531c943e6f93656b42b
hseka <sekaherve@gmail.com>
2020-04-13 17:13:21 +0200

    Mise en place du Plugin. Il est fonctionnel

--- 06_JavaScript/01_01_Form_MultiSelect/js/script.js
+++ 06_JavaScript/01_01_Form_MultiSelect/js/script.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
 
 
 });
+
+function listbox_selectall(listID, isSelect) {
+    var listbox = document.getElementById(listId);
+
+    for(var count=0; count < listbox.options.length; count++) {
+        listbox.options[count].selected = isSelect;
+    }
+}
